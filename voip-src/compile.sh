44b196a51ae345398200de3a93f3a81e0ac844ef
Hiroaki KARASAWA <siqvare@gmail.com>
2017-07-03 05:08:14 +0900

    Compile as C++ 11

--- voip-src/compile.sh
+++ voip-src/compile.sh
@@ -1,4 +1,4 @@
-c++ server.cpp -o server -Wall -O3
-c++ client.cpp -o client -Wall -O3
+c++ -std=c++11 server.cpp -o server -Wall -O3 -pthread
+c++ -std=c++11 client.cpp -o client -Wall -O3 -pthread
 mv server ../bin/
 mv client ../bin
